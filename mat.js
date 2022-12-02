0e1ed38cc1c07c672411ce1d65714988fa789bfc
Marce Dominguez <marcedominguez95@outlook.com>
2022-12-02 19:33:58 -0300

    :sparkles:
    operaciones - back

---
+++ mat.js
@@ -0,0 +1,6 @@
+const adicion = (a, b) => a + b;
+const sustraccion = (a, b) => a - b;
+const multiplicacion = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+module.exports = {adicion, sustraccion, multiplicacion, division}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 
 // Node - Backend - Corre en un servidor, NO en browser (front)
-console.log('Hola wachin')
+console.log('Hola wey')
 // Objeto global- me devuelve un obj global c/ ciertos atributos
 //console.log(global);
 //modulos para explorar
@@ -10,10 +10,27 @@ console.log('Hola wachin')
 
 const os = require('os');
 const path = require('path');
+const {adicion, sustraccion, multiplicacion, division} = require('./mat')
 
+console.log(adicion(5, 8))
+console.log(sustraccion(10, 3))
+console.log(multiplicacion(7, 8))
+console.log(division(200, 2))
+
+
+
+
+/*
 console.log(os.type())
 console.log(os.version())
 console.log(os.homedir())
 
 console.log(__dirname)
 console.log(__filename)
+
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))
+*/
